4603f2ff3a583c94ab4df5f044a3874169af49a6
Parvez Mir <parvezmir@Parvezs-MacBook-Air.local>
2024-09-01 18:17:14 +0530

    changed socket url

--- components/web-socket-provider.jsx
+++ components/web-socket-provider.jsx
@@ -1,9 +1,11 @@
 'use client'; // This marks the component as a client component
 
+import useStorage from '@/hooks/useStorage';
 import { useWebSocketWithSound, AlertBox } from '@/hooks/useWebSocket';
 
 export default function WebSocketProvider() {
-  const { alert, audioRef, handleAlert } = useWebSocketWithSound('ws://13.127.133.23:8000/ws/notifications/1');
+  const {getItem} = useStorage();
+  const { alert, audioRef, handleAlert } = useWebSocketWithSound(`ws://13.127.133.23:8000/ws/notifications/${getItem('company_name')}/${getItem('user_id')}`);
 
   return (
     <>

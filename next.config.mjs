ebb9053699722468de458f37e340e6bc748d1cfe
nikhil-springtown <nikhil@springtown.ai>
2024-09-05 06:31:38 +0000

    path-changed-for-api

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     env: {
-        BACKEND_API: "http://13.127.133.23:8000/",
+        // BACKEND_API: "http://13.127.133.23:8000/api/",
+        BACKEND_API: "https://api.testsite.name/api/",
+        // AI_SUMMARY_API: "http://13.127.133.23:5000/",
         AI_SUMMARY_API: "http://13.127.133.23:5000/",
     },
 };
